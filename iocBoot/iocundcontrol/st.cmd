6c0c384887ed9e2942766dc341bd0ded8e3b6ac2
Stuart Wilkins <swilkins@bnl.gov>
2014-11-09 08:29:11 -0500

    Working verion

--- iocBoot/iocundcontrol/st.cmd
+++ iocBoot/iocundcontrol/st.cmd
@@ -3,16 +3,34 @@
 ## You may have to change undcontrol to something else
 ## everywhere it appears in this file
 
-#< envPaths
+< envPaths
 
 ## Register all support components
 dbLoadDatabase("../../dbd/undcontrol.dbd",0,0)
 undcontrol_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-dbLoadRecords("../../db/undcontrol.db","user=swilkins")
+dbLoadRecords("${TOP}/db/interp.db","Sys=XF:23ID-ID,Dev={EPU:1-RLK},N=1000000,In=Val:Gap-I,Out=Enrgy-I,DOL=SR:C23-ID:G1A{EPU:1-Ax:Gap}-Mtr.RBV")
+dbLoadRecords("${TOP}/db/interp.db","Sys=XF:23ID-ID,Dev={EPU:1-FLK},N=1000000,In=Enrgy-SP,Out=Val:Gap-SP")
+
+system("install -m 777 -d $(TOP)/as/save") 
+system("install -m 777 -d $(TOP)/as/req")
+
+## autosave/restore machinery
+save_restoreSet_Debug(0)
+save_restoreSet_IncompleteSetsOk(1)
+save_restoreSet_DatedBackupFiles(1)
+
+set_savefile_path("${TOP}/as","/save")
+set_requestfile_path("${TOP}/as","/req")
+
+set_pass0_restoreFile("info_positions.sav")
+set_pass1_restoreFile("info_settings.sav")
 
 iocInit()
 
-## Start any sequence programs
-#seq sncundcontrol,"user=swilkins"
+cd ${TOP}/as/req
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5 , "")
+create_monitor_set("info_settings.req", 15 , "")
+
--- undcontrolApp/src/interp.c
+++ undcontrolApp/src/interp.c
@@ -1,16 +1,41 @@
 #include <registryFunction.h>
 #include <epicsExport.h>
 #include <aSubRecord.h>
+#include <stdio.h>
 
-static long my_asub_routine(aSubRecord *prec) {
-  long i, *a;
-  double sum=0;
+static long interp1d(aSubRecord *prec) {
+  double *a, *e, *vala;
+  double x_start, x_step, x_n, frac, r;
+  long n;
 
-  a = (long *)prec->a;
-  for (i=0; i<prec->noa; i++) {
-    sum += a[i];
+  /* 
+   * This routine does the 1d interpolation. The format of the waveform
+   * wf[0] = number of elements
+   * wf[1] = x start
+   * wf[2] = x step
+   *
+   * a is the input value
+   * e is the interp table
+   */
+
+  a = (double *)prec->a;
+  e = (double *)prec->e;
+  vala = (double *)prec->vala;
+
+  x_start = e[0];
+  x_step = e[1];
+  x_n = e[2];
+
+  frac = ((a[0] - x_start) / x_step);
+  n = (long)frac;
+  r = frac - n;
+  if((n < 0) || (n > (x_n-1))){
+    return 1;
   }
+
+  vala[0] = (e[n+4] - e[n+3]) * r;
+  vala[0] = vala[0] + e[n+3];
   return 0; /* process output links */
 }
 
-epicsRegisterFunction(my_asub_routine);
+epicsRegisterFunction(interp1d);
