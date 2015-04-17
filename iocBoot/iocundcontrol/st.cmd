d7f99be5077e578b2904f2567378c0bdb78ebe6d
Daron Chabot <dchabot@bnl.gov>
2015-04-17 16:56:14 -0400

    Fix autosave restoration problem.

--- iocBoot/iocundcontrol/st.cmd
+++ iocBoot/iocundcontrol/st.cmd
@@ -37,6 +37,7 @@ set_savefile_path("${TOP}/as","/save")
 set_requestfile_path("${TOP}/as","/req")
 
 set_pass0_restoreFile("info_positions.sav")
+set_pass0_restoreFile("info_settings.sav")
 set_pass1_restoreFile("info_settings.sav")
 
 asSetFilename("/epics/xf/23id/xf23id.acf")
